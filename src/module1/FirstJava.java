3c7a84d9f9b3eccea62b05cf8b1f396956ac24a7
abhresh <tabhresh@gmail.com>
2018-07-25 21:36:27 +0530

    First Commit

---
+++ src/module1/FirstJava.java
@@ -0,0 +1,14 @@
+package module1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
